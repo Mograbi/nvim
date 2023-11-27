a95ce301be5c34e51852b348f9d7926a9626f898
Mograbi <mograbi.moawiya@gmail.com>
2023-11-27 10:50:23 +0200

    added typescript and other changes

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,104 @@
+local plugins = {
+  { "dmmulroy/tsc.nvim", lazy = false},
+  { "tpope/vim-fugitive", lazy = false},
+  { 'pwntester/octo.nvim', lazy = false},
+  { "github/copilot.vim", lazy = false},
+  {"nvim-tree/nvim-web-devicons", lazy = false},
+  {
+  "neovim/nvim-lspconfig",
+   config = function()
+      require'lspconfig'.tsserver.setup {}
+   end,
+  },
+  {
+
+"smoka7/hop.nvim",
+
+keys = {
+
+{
+
+",",
+
+function()
+
+require("hop").hint_words()
+
+end,
+
+mode = { "n", "x", "o" },
+
+},
+
+{
+
+"s",
+
+function()
+
+require("hop").hint_char2()
+
+end,
+
+mode = { "n", "x", "o" },
+
+},
+
+{
+
+"f",
+
+function()
+
+require("hop").hint_char1 {
+
+direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+
+current_line_only = true,
+
+}
+
+end,
+
+mode = { "n", "x", "o" },
+
+},
+
+{
+
+"F",
+
+function()
+
+require("hop").hint_char1 {
+
+direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+
+current_line_only = true,
+
+}
+
+end,
+
+mode = { "n", "x", "o" },
+
+},
+
+},
+
+opts = {
+
+multi_windows = true,
+
+keys = "htnsueoaidgcrlypmbkjvx",
+
+uppercase_labels = true,
+
+-- reverse_distribution = true,
+
+},
+
+},
+}
+
+return plugins
